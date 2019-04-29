4344a801c46d37bfefa50e674d6500b7c90c4524
mracar07 <umutcan@umutcanacar.me>
2019-04-29 21:14:40 +0000

    Fix Kernel building
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -169,6 +169,8 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
         KERNEL_CLANG_VERSION := $(LLVM_PREBUILTS_VERSION)
     endif
     TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)/bin
+    KBUILD_COMPILER_STRING := $(shell $(TARGET_KERNEL_CLANG_PATH)/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+    export KBUILD_COMPILER_STRING
     ifeq ($(KERNEL_ARCH),arm64)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
     else ifeq ($(KERNEL_ARCH),arm)
@@ -316,3 +318,4 @@ kernel: $(INSTALLED_KERNEL_TARGET)
 dtbo: $(INSTALLED_DTBOIMAGE_TARGET)
 
 endif # TARGET_NO_KERNEL
+
